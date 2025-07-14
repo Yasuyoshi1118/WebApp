8477ad94c0ed160a8b9752de7c20230a24468966
yasuyoshi1118 <yasu1118h@gmail.com>
2025-07-14 21:27:23 +0900

    push_helloworld

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = 'Guest'
+    if request.method == 'POST':
+        name = request.form.get('name', 'Guest')
+    return render_template('index.html', name=name)
+
+if __name__ == '__main__':
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
